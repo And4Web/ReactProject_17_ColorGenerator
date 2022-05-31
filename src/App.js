7d11a884f4af608dacd7233109007733d031efc4
And4Web <anands2807@gmail.com>
2022-05-31 20:36:01 +0530

    basic structure created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+import Values from "values.js";
+
 import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <>
+      <section>
+        <h3>Generate your favorite color's shades</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="#f15025"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </section>
+      <section>
+        <h4>List goes here:</h4>
+      </section>
+    </>
   );
 }
 
